34a63e08e6a5e65035ee2585de7039124c943432
Amos <amos.langat@student.moringaschool.com>
2022-08-12 15:20:23 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Code your solutions in this file
+// let name = ["Amos", "Bianca", "Cassie"];
+// let messages = [];
+
+// function writeCards([name], event){
+//     for (let i = 0; i > name.length; i+=1) {
+//         messages.push (`Thank you ${name[0]} for the wonderful  ${event} gift!`);
+//     }
+//     return messages;
+// }
+const names = ["Amos", "Bianca", "Cassie"];
+ function writeCards(names, event) {   let messages = [];
+  names.forEach((name,) =>
+{
+messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+  });
+  return messages;
+}  console.log(writeCards(names));
+
+
+// you write comments in JavaScript with two forward slashes
+// i is the integer parameter of your countdown function
+// i is passed to countdown when called, i.e. countdown(9)
+function countdown(i)    
+{
+    let number = "";   
+
+    while( i >= 0)
+    {    
+        number += i;
+        if(i > 0) {
+            console.log(number); 
+        }
+        i--;
+    }
+    return number;
+}
+let result = countdown(10);  
+
+
+console.log(result);
+
+function countDown(i) {
+  while (i >= 0) {
+      console.log(i);
+      i--;
+  }
+}
+countDown(10);
